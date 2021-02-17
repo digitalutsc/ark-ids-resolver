50ce55b1cbb15da8be6002cc4f042468a0d262ef
Kyle Huynh <kyle.huynh@utoronto.ca>
2021-02-17 12:06:41 -0500

    added database config

---
+++ config/MysqlArkConf.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ark\Resolver\Library;
+
+class MysqlArkConf
+{
+    static public $mysql_host = '';
+    static public $mysql_user = '';
+    static public $mysql_passwd = '';
+    static public $mysql_dbname = ''; // please enter the name of database which you have just created.
+    static public $mysql_port = 3306;
+}
+
